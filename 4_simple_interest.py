dff5dc505b94dcb66cb8cd6637021ea870829fc6
Komali Boda <bodakomali12@gmail.com>
2026-06-04 05:05:00 +0530

    Create 4_simple_interest.py

---
+++ 4_simple_interest.py
@@ -0,0 +1,20 @@
+# Simple intrest program
+ 
+amount = int(input("enter amount : "))
+time = int(input("enter time : "))
+interest = int(input("enter intrest rate: "))
+ 
+simple_interest = (amount*time*interest)/100
+total = amount+simple_interest
+print("Total Amount: ", total)
+
+#Advanced simple intrest
+
+amount = int(input("enter amount : "))
+month = int(input("enter months : "))
+days = int(input("enter days : "))
+time = month+days
+interest = int(input("enter intrest rate: "))
+simple_interest = (amount*time*interest)/100
+total = amount+simple_interest
+print("Total Amount: ", total)

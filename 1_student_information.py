a69849dfb95da278d851387be7ffc2eaff030c96
Komali Boda <bodakomali12@gmail.com>
2026-06-03 09:15:18 +0530

    Create 1_student_information.py

---
+++ 1_student_information.py
@@ -0,0 +1,16 @@
+# Student information program 
+
+# storing student details in variables 
+
+name = "Komali"
+roll_no = 1
+age = 18
+branch = "Data Science "
+college = "XYZ college"
+
+# displaying student details 
+print("Student Name: ", name)
+print("Roll No: ", roll_no)
+print("Age: ", age)
+print("Branch: ", branch)
+print("College: ", college)

af2ad2ca7131ebe81664fab463876092a2d2bef4
Komali Boda <bodakomali12@gmail.com>
2026-06-04 04:41:26 +0530

    Create 3_area_of_rectangle.py

---
+++ 3_area_of_rectangle.py
@@ -0,0 +1,9 @@
+# Area of rectangle 
+ 
+# Taking data from the user
+ 
+length = int(input("enter length  : "))
+width = int(input("enter width : "))
+ 
+area = length*width
+print("Area of rectangle : ", area)

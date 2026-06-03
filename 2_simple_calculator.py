bf0993ac9ae86ceb5aa8b8e61edaafad43597d00
Komali Boda <bodakomali12@gmail.com>
2026-06-03 09:19:31 +0530

    Create 2_simple_calculator.py

---
+++ 2_simple_calculator.py
@@ -0,0 +1,16 @@
+# Simple calculator program 
+
+# Taking input from the user
+
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+# Performing operation 
+print("Addition: ", a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Division: ", a/b )
+print("Modulus: ", a%b)
+print("Floor division: ",a//b)
+print("Power: ", a**b)
+

668cecc778b1d26d09be2e9c05dcaad25c602ade
Komali Boda <bodakomali12@gmail.com>
2026-06-04 05:33:21 +0530

    Create 6_percentage_calculator.py

---
+++ 6_percentage_calculator.py
@@ -0,0 +1,10 @@
+# percentage calculator 
+
+maths = int(input("Enter marks: "))
+science = int(input("Enter marks: "))
+physics = int(input("Enter marks: "))
+total_marks = maths+science+physics 
+average = total_marks/3
+percentage = (total_marks/300)*100
+print("Average: ",average )
+print("percentage: ", percentage,"%" )

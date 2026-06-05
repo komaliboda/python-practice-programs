407df9b5f8285e9c3373e61cd96161e766e2b571
Komali Boda <bodakomali12@gmail.com>
2026-06-06 05:20:41 +0530

    Update 4_simple_interest.py

--- 4_simple_interest.py
+++ 4_simple_interest.py
@@ -11,8 +11,8 @@ print("Total Amount: ", total)
 #Advanced simple intrest
 
 amount = int(input("enter amount : "))
-month = int(input("enter months : "))
-days = int(input("enter days : "))
+month = int(input("enter months : "))/12
+days = int(input("enter days : "))/365
 time = month+days
 interest = int(input("enter intrest rate: "))
 simple_interest = (amount*time*interest)/100

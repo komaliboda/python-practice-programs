3574087821dfd4c4b3ac26c7d03a2291f976c51a
Komali Boda <bodakomali12@gmail.com>
2026-06-07 05:22:31 +0530

    Create 15_login_system.py

---
+++ 15_login_system.py
@@ -0,0 +1,10 @@
+# login system 
+
+user_password = "komali123"
+
+password = input("enter your password 🔑 : ")
+
+if user_password == password:
+    print("login successfully")
+else:
+    print("wrong password ")

fee98624fce780e7034df5fffadbc9d73924e3c3
Komali Boda <bodakomali12@gmail.com>
2026-06-07 04:40:57 +0530

    Create 13_grade_calculator.py

---
+++ 13_grade_calculator.py
@@ -0,0 +1,15 @@
+# Grading calculator 
+
+marks = int(input("enter your marks: "))
+if marks < 0 or marks > 100:
+    print("Invalid marks")
+elif  marks >= 90 :
+    print("A Grade")
+elif marks >= 80:
+    print("B Grade")
+elif marks >= 60:
+    print("C Grade")
+elif marks >= 40:
+    print("D Grade ")
+else:
+    print("Fail")

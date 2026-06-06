56186c82b8817d2fdc5b1979045baa16f7adda9a
Komali Boda <bodakomali12@gmail.com>
2026-06-06 11:42:40 +0530

    Create 9_positive_negative.py

---
+++ 9_positive_negative.py
@@ -0,0 +1,11 @@
+# checking whether the number  is positive negative or zero 
+
+# Taking a number from the user
+num = int(input("enter a number: "))
+
+if num > 0:
+    print("The number is positive")
+elif num < 0:
+    print("The number is negative")
+else: 
+    print("The number is zero ")

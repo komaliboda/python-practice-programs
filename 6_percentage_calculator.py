192d9fbc36b0b0604b1357c53eccb52d800b935c
Komali Boda <bodakomali12@gmail.com>
2026-07-03 19:06:46 +0530

    Update 6_percentage_calculator.py

--- 6_percentage_calculator.py
+++ 6_percentage_calculator.py
@@ -1,8 +1,8 @@
 # percentage calculator 
 
-maths = int(input("Enter marks: "))
-science = int(input("Enter marks: "))
-physics = int(input("Enter marks: "))
+maths = int(input("Enter  maths marks: "))
+science = int(input("Enter science marks: "))
+physics = int(input("Enter physics marks: "))
 total_marks = maths+science+physics 
 average = total_marks/3
 percentage = (total_marks/300)*100

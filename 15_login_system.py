efbd01374383d189c5a6310607417b40dc926e82
Komali Boda <bodakomali12@gmail.com>
2026-07-09 23:34:40 +0530

    Update 15_login_system.py

--- 15_login_system.py
+++ 15_login_system.py
@@ -11,3 +11,19 @@ if username == "komali" and password == "komali123":
     print("welcome" ,username)
 else:
     print("Invalid username or password")
+
+
+
+# Login System
+# This program verifies the username and password using nested if statements.
+
+user_name = input("Enter  user name: ")
+
+if user_name == "komali":
+    password = input("Enter your password ")
+    if password == "komali@112":
+        print("Login Successfully ")
+    else:
+        print("wrong password")
+else:
+    print("User name not found")

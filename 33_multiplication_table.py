14391c753530a9a3d1df91ddb50e6b0878c2631a
Komali Boda <bodakomali12@gmail.com>
2026-07-13 19:21:01 +0530

    Create 33_multiplication_table.py

---
+++ 33_multiplication_table.py
@@ -0,0 +1,6 @@
+# Multiplication Table
+# This program prints the multiplication table of a given number using a for loop.
+
+num = int(input("Enter a num:"))
+for i in range(1,11):
+    print(num ,"x",i,"=",num*i)

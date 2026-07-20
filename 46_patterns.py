994330e23cc85ec4fb3c653f9f956172739eca18
Komali Boda <bodakomali12@gmail.com>
2026-07-20 19:40:23 +0530

    Update 46_patterns.py

--- 46_patterns.py
+++ 46_patterns.py
@@ -9,3 +9,9 @@ for i in range(1,6):
     for j in range(i):
         print(i,end = " ")
     print()
+
+# pattern 3 : decreasing the number pattern 
+for i in range(5,0,-1):
+    for j in range(i,0,-1):
+        print(j,end = " ")
+    print()

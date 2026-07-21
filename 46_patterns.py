34799caf9d9faf8d761343c708544ff3193b2474
Komali Boda <bodakomali12@gmail.com>
2026-07-21 20:01:35 +0530

    Update 46_patterns.py

--- 46_patterns.py
+++ 46_patterns.py
@@ -45,3 +45,9 @@ for i in range(1,6):
     for j in range(1,7-i):
         print(j,end = " ")
     print()
+
+# Pattern 9: Inverted Decreasing Number Pattern
+for i in range(6):
+    for j in range(5,i,-1):
+        print(j,end =" ")
+    print()

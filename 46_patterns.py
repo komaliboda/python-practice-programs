15826329fb65fba969346869b70462949d61898b
Komali Boda <bodakomali12@gmail.com>
2026-07-21 08:00:01 +0530

    Update 46_patterns.py

--- 46_patterns.py
+++ 46_patterns.py
@@ -15,3 +15,9 @@ for i in range(5,0,-1):
     for j in range(i,0,-1):
         print(j,end = " ")
     print()
+
+# Pattern 4: Reverse Increasing Triangle
+for i in range(5,0,-1):
+    for j in range(5,i-1,-1):
+        print(j,end = " ")
+    print()

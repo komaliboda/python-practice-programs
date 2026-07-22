5841b485fb0c10cf3cbd3a78a4fd3099d64ef149
Komali Boda <bodakomali12@gmail.com>
2026-07-22 19:00:02 +0530

    Update 46_patterns.py

--- 46_patterns.py
+++ 46_patterns.py
@@ -69,4 +69,12 @@ for i in range(1,6):
     for j in range(1,i+1):
         print(chr(64+j),end= " ")
     print()
+
+# Pattern 13: Floyd's Triangle
+num = 1
+for i in range(1,6):
+    for j in range(i):
+        print(num,end = " ")
+        num = num+1
+    print()
     

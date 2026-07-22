00b7a7c838afe9c5b0c5b969e99b9ea980fe9073
Komali Boda <bodakomali12@gmail.com>
2026-07-22 19:46:20 +0530

    Update 46_patterns.py

--- 46_patterns.py
+++ 46_patterns.py
@@ -90,11 +90,20 @@ for i in range(1,6):
     for j in range(i):
         print(chr(64+i),end = " ")
     print()
+
 # Pattern 16: Expanding Increasing Alphabet Triangle (Reverse Start)
 for i in range(5,0,-1):
     for j in range(i,6):
         print(chr(64+j),end = " ")
     print()
+
+# Pattern 17: Inverted Increasing Alphabet Triangle
+for i in range(6,1,-1):
+    for j in range(1,i):
+        print(chr(64+j),end = " ")
+    print()
+    
+
     
 
     

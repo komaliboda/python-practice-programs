90eb5407a843fccfe23b473412630092baf10784
Komali Boda <bodakomali12@gmail.com>
2026-07-22 19:51:05 +0530

    Update 46_patterns.py

--- 46_patterns.py
+++ 46_patterns.py
@@ -102,6 +102,12 @@ for i in range(6,1,-1):
     for j in range(1,i):
         print(chr(64+j),end = " ")
     print()
+
+# Pattern 18: Increasing Star Triangle
+for i in range(1,6):
+    for j in range(i):
+        print("*" ,end=" ")
+    print()
     
 
     

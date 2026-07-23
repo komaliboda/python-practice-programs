2e862b02666aa512286dbe1f9269a8128d579cec
Komali Boda <bodakomali12@gmail.com>
2026-07-23 21:05:58 +0530

    Update 46_patterns.py

--- 46_patterns.py
+++ 46_patterns.py
@@ -119,7 +119,7 @@ for i in range(1,6):
 
 for i in range(1,6):
     for j in range(5,i,-1):
-        print(" ",end = "")
+        print("",end = "")
     for k in range(i):
         print("*" ,end=" ")
     print()

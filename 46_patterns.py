d4191a9509e55611af57ccd20b478c35a0ed7ff5
Komali Boda <bodakomali12@gmail.com>
2026-07-23 21:24:45 +0530

    Update 46_patterns.py

--- 46_patterns.py
+++ 46_patterns.py
@@ -123,6 +123,14 @@ for i in range(1,6):
     for k in range(i):
         print("*" ,end=" ")
     print()
+
+# Pattern 21: Inverted Right-Aligned Star Triangle
+for i in range(1,6):
+    for j in range(1,i):
+        print("",end = " ")
+    for k in range(6,i,-1):
+        print("*",end = " ")
+    print()
     
 
     

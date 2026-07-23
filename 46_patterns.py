ea4bef687189c32258ba4a11d48ab5ec99fe95e7
Komali Boda <bodakomali12@gmail.com>
2026-07-23 22:19:09 +0530

    Update 46_patterns.py

--- 46_patterns.py
+++ 46_patterns.py
@@ -138,6 +138,14 @@ for i in range(1,6):
     for k in range(2*i-1):
         print("*",end = " ")
     print()
+
+# Pattern 23: Inverted Full Pyramid Star Pattern
+for i in range(4,-1,-1):
+    for j in range(5,i,-1):
+        print(" ",end = " ")
+    for k in range(2 *i + 1):
+        print("*",end = " ")
+    print()
     
 
     

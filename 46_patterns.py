d6a0a505c013f7009eebcb58bd885ea1cfe1cec8
Komali Boda <bodakomali12@gmail.com>
2026-07-23 22:00:22 +0530

    Update 46_patterns.py

--- 46_patterns.py
+++ 46_patterns.py
@@ -131,6 +131,13 @@ for i in range(1,6):
     for k in range(6,i,-1):
         print("*",end = " ")
     print()
+# Pattern 22: Full Pyramid Star Pattern
+for i in range(1,6):
+    for j in range(5,i,-1):
+        print("",end = " ")
+    for k in range(2*i-1):
+        print("*",end = " ")
+    print()
     
 
     

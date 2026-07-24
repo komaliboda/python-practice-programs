1c4cdac514634f40d1ddc621a835f13ae29a3699
Komali Boda <bodakomali12@gmail.com>
2026-07-24 21:37:35 +0530

    Update 46_patterns.py

--- 46_patterns.py
+++ 46_patterns.py
@@ -146,6 +146,21 @@ for i in range(4,-1,-1):
     for k in range(2 *i + 1):
         print("*",end = " ")
     print()
+
+# Pattern 24: Diamond Star Pattern
+
+for i in range(1,6):
+    for j in range(6,i,-1):
+        print(" ",end = " ")
+    for k in range(2*i-1):
+        print("*",end = " ")
+    print()
+for i in range(3,-1,-1):
+    for j in range(5,i,-1):
+        print(" ",end = " ")
+    for k in range(2*i+1):
+        print("*",end = " ")
+    print()
     
 
     

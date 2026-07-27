62b16f4ff58759ef63fe14264b79b87a7a184793
Komali Boda <bodakomali12@gmail.com>
2026-07-27 18:26:12 +0530

    Update 46_patterns.py

--- 46_patterns.py
+++ 46_patterns.py
@@ -267,6 +267,15 @@ for i in range(5):
         else:
             print(" ",end = " ")
     print()
+# Pattern 34: 0-1 Triangle Pattern
+
+for i in range(1,6):
+    for j in range(i):
+        if (i + j) % 2 == 0:
+            print(0, end=" ")
+        else:
+            print(1, end=" ")
+    print()
     
     
         

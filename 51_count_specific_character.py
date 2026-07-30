ca487809d48f6c08b89c9640cb5a2a6b6c4e134f
Komali Boda <bodakomali12@gmail.com>
2026-07-30 19:18:00 +0530

    Create 51_count_specific_character.py

---
+++ 51_count_specific_character.py
@@ -0,0 +1,10 @@
+# counting a specific character 
+
+s = input("enter something: ").lower()
+specific_char = input("Enter a specific character: ").lower()
+count = 0
+for ch in s:
+    if ch in specific_char:
+        count += 1
+print(count)
+    

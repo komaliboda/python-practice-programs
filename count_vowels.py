661622383824ce1ef5362ecac0b95900ea7dc9af
Komali Boda <bodakomali12@gmail.com>
2026-07-30 18:22:49 +0530

    Delete count_vowels.py

--- count_vowels.py
+++
@@ -1,9 +0,0 @@
-# Count the number of vowels in a string
-
-s = input("enter something:").lower()
-vowels = "aeiou"
-count = 0
-for i in s:
-    if i in vowels:
-        count += 1
-print("The count is :",count)

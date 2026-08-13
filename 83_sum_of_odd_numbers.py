84f39853e5323221a186eb5380b3cc5787c72609
Komali Boda <bodakomali12@gmail.com>
2026-08-13 19:53:56 +0530

    Create 83_sum_of_odd_numbers.py

---
+++ 83_sum_of_odd_numbers.py
@@ -0,0 +1,14 @@
+# Find the sum of odd numbers in a list
+
+num = [10,20,30,15,17]
+total = 0 
+
+for i in num:
+    if i % 2 != 0:
+        total = total + i
+
+print(total )
+    
+    
+    
+    

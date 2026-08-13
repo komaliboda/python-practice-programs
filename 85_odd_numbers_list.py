a05fcc0e76172a33aec6c82c4108475653465e70
Komali Boda <bodakomali12@gmail.com>
2026-08-13 20:01:45 +0530

    Create 85_odd_numbers_list.py

---
+++ 85_odd_numbers_list.py
@@ -0,0 +1,14 @@
+# Create a list containing only odd numbers
+
+num = [10,20,30,15,17]
+odd_list = []
+
+for i in num:
+    if i % 2 != 0:
+        odd_list.append(i)
+
+print(odd_list)
+    
+    
+    
+    

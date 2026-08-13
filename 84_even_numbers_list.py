65dbcbf4dd95cd4282308aa1b08dd8eb2d42f2fa
Komali Boda <bodakomali12@gmail.com>
2026-08-13 19:59:48 +0530

    Create 84_even_numbers_list.py

---
+++ 84_even_numbers_list.py
@@ -0,0 +1,14 @@
+# Create a list containing only even numbers
+
+num = [10,20,30,15,17]
+even_list = []
+
+for i in num:
+    if i % 2 == 0:
+        even_list.append(i)
+
+print(even_list)
+    
+    
+    
+    

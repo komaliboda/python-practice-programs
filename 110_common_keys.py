d00dfa7aa1f9e1378caf2ca38f6b9f48e6e2da69
Komali Boda <bodakomali12@gmail.com>
2026-08-20 07:45:33 +0530

    Create 110_common_keys.py

---
+++ 110_common_keys.py
@@ -0,0 +1,9 @@
+# Find common keys between two dictionaries
+
+dict1 = {"a":10,"b":20,"c":30}
+dict2 = {"d":20,"b":300,"a":10}
+temp = {}
+for key,values in dict1.items():
+    if key in dict2:
+        temp[key] = values
+print(temp)

391b627359125fdcf638aeaed35f48461a770dfa
Komali Boda <bodakomali12@gmail.com>
2026-08-29 19:28:50 +0530

    Create 126_power_recursion.py

---
+++ 126_power_recursion.py
@@ -0,0 +1,8 @@
+# Calculate the power of a number using recursion
+
+def power(base,exponent):
+    if exponent == 0:
+        return 1
+    return base * power(base,exponent-1)
+
+print(power(2,3))

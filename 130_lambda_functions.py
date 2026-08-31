64f77d34eac63be0e49a61e39ea237ab7149ba23
Komali Boda <bodakomali12@gmail.com>
2026-08-31 18:51:44 +0530

    Create 130_lambda_functions.py

---
+++ 130_lambda_functions.py
@@ -0,0 +1,9 @@
+# Calculate the square of a number using a lambda function
+
+square = lambda x:x * x
+print(square(5))
+
+# add the numbers using parameters and lambda function 
+
+add = lambda a,b: a + b
+print(add(10,20))
